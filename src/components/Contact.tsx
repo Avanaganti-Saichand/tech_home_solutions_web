75a60bfe45bd8dbf22b4b4f2daf65bd027d2ea56
Saichand <saichand98498@gmail.com>
2025-03-26 13:23:55 -0500

    Fixed contact us form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -115,7 +115,7 @@ export default function Contact() {
               <span className="font-bold">Phone:</span>{" "}
               <a
                 href="tel:+917396507394"
-                className="underline hover:text-white transition"
+                className="underline hover:text-[#F4A300] transition"
               >
                 +91 7396507394
               </a>
@@ -125,7 +125,7 @@ export default function Contact() {
               <span className="font-bold">Email:</span>{" "}
               <a
                 href="mailto:techhomehyd@gmail.com"
-                className="underline hover:text-white transition"
+                className="underline hover:text-[#F4A300] transition"
               >
                 techhomehyd@gmail.com
               </a>
